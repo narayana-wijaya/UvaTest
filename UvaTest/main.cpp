49e02ab4b7444d37e799c84ccc70c491fea423da
Narayana Wijaya <narayana.wijaya@geekseat.com.au>
2018-05-12 07:56:07 +0700

    Initial Commit

---
+++ UvaTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UvaTest
+//
+//  Created by Narayana Wijaya on 12/05/18.
+//  Copyright © 2018 Nara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
